e7cb53875b27cbed48aeb8aa97eba343994a44e5
Anastasiia Radasheva <eha20082@gmail.com>
2025-11-18 09:59:09 +0200

    php lehe struktuur on määratud

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
-<?php
-    echo "<h1>";
-    echo "Tere hommikust";
-    echo "</h1>";
-    $tekst = "TARpv24 õpilane";
-    echo $tekst;
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP tööd</title>
+    <link rel="stylesheet" href="styleCSS.css">
+</head>
+<body>
 
+<?php
+//päis
+include("header.php");
+?>
+<?php
+//navigeerimidmenüü
+include("nav.php");
+?>
+<main>
+    <section>
+
+    </section>
+</main>
+
+<?php
+//jalus
+include("footer.php");
+?>
+
+
+</body>
+</html>
 
 
 
---
+++ nav.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+        <li><a href="?link=avaleht.php">Avaleht</a></li>
+        <li><a href="?link=too1.php">Töö</a>
+        </li>
+        <li><a href="?link=gitKasutamine.php">Git kasud</a>
+        </li>
+        <li><a href="https://anastasiiaradasheva24.thkit.ee/">Vana index</a></li>
+    </ul>
+</nav>
