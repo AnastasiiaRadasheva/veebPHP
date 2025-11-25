2fb196909217dfe39c4a0bfd1de505d4ae8a716d
Anastasiia Radasheva <eha20082@gmail.com>
2025-11-25 11:59:07 +0200

    uus fail on pildid php ja uus branch;

---
+++ content/pildid.php
@@ -0,0 +1,76 @@
+<?php
+function clearVarsExcept($url, $varname)
+{
+    $url = basename($url);
+    if(str_starts_with($url, '?')){
+        return "?$varname=".$_REQUEST[$varname];
+    }
+    return strtok($url, "?")."?$varname=".$_REQUEST[$varname];
+}
+?>
+<form name ="pilt" action="<?=clearVarsExcept($_SERVER['REQUEST_URI'], "link")?>" method = "post"
+  <select name="pildid">
+    <option value="">Vali pilt</option>
+<?php
+		$kataloog = 'pildid';
+		$asukoht=opendir($kataloog);
+		while($rida = readdir($asukoht)){
+			if($rida!='.' && $rida!='..'){
+				echo "<option value='$rida'>$rida</option>n";
+			}
+		}
+?>
+  </select>
+  <input type="submit" value="Vaata">
+</form>
+<?php
+
+if (!empty($_POST['pildid'])) {
+
+    $pilt = $_POST['pildid'];
+    $pildi_aadress = 'pildid/' . $pilt;
+
+    // loeme pildi info
+    $pildi_andmed = getimagesize($pildi_aadress);
+
+    $laius = $pildi_andmed[0];
+    $korgus = $pildi_andmed[1];
+    $formaat = $pildi_andmed[2];
+
+    // max mõõdud pisipildile
+    $max_laius = 120;
+    $max_korgus = 90;
+
+    // ---------------------------
+    //    Suhte arvutamine
+    // ---------------------------
+    if ($laius <= $max_laius && $korgus <= $max_korgus) {
+        $ratio = 1;
+    } elseif ($laius > $korgus) {
+        $ratio = $max_laius / $laius;
+    } else {
+        $ratio = $max_korgus / $korgus;
+    }
+
+    // ---------------------------
+    //    Pisipildi mõõdud
+    // ---------------------------
+    $pisi_laius = round($laius * $ratio);
+    $pisi_korgus = round($korgus * $ratio);
+
+    // ---------------------------
+    //    Väljund
+    // ---------------------------
+    echo "<h3>Originaal pildi andmed</h3>";
+    echo "Laius: $laius px<br>";
+    echo "Kõrgus: $korgus px<br>";
+    echo "Formaat: $formaat<br>";
+
+    echo "<h3>Uue pildi andmed</h3>";
+    echo "Arvutatud suhe: $ratio<br>";
+    echo "Laius: $pisi_laius px<br>";
+    echo "Kõrgus: $pisi_korgus px<br><br>";
+
+    echo "<img width='$pisi_laius' height='$pisi_korgus' src='$pildi_aadress'><br>";
+}
+?>
